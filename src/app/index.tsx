ce3181cc3180ed7ec858494667775fdc6d984e11
yRizer <taynan28112003@gmail.com>
2025-09-20 17:42:22 -0300

    Redirecionamento ao clicar no botão de Ler QR Code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,11 @@
 
+import Icons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useRouter } from "expo-router";
 import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TopBar from "../components/topBar";
-import Icons from '@expo/vector-icons/MaterialCommunityIcons';
+
+const router = useRouter();
 
 export default function Index() {
     return (
@@ -14,10 +17,9 @@ export default function Index() {
                     <Text style={style.titulo}>Seja bem vindo</Text>
                     <Text style={style.subtitulo}>Descubra o melhor da nossa cidade no seu ritmo. Comece sua jornada agora!</Text>
                 </View>
-
                
                 <View style={style.containerQrCode}>
-                    <TouchableOpacity style={style.btnQrCode}>
+                    <TouchableOpacity style={style.btnQrCode} onPress={() => router.push('/camera')}>
                         
                         <Icons name="qrcode-scan" size={150} color="#1C1C1E" />
                         <Text style={style.textoQrCode}>Ler QR Code</Text>
